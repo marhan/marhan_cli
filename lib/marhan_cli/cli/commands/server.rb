aeb0816880211646ac5f5bfac3b1623c1960d19b
Markus Hanses <markus@markushanses.de>
2012-10-23 21:38:11 +0200

    copy ssh pub id to remote host.

--- lib/marhan_cli/cli/commands/server.rb
+++ lib/marhan_cli/cli/commands/server.rb
@@ -1,3 +1,5 @@
+require 'net/ssh'
+
 module MarhanCli
   class Cli
     include Thor::Actions
@@ -6,8 +8,30 @@ module MarhanCli
 
     def add_ssh_key(user, password, host)
       #https://gist.github.com/1639381
-      say ""
-      run "ssh -l #{user} -L#{host} -p 2222 || ls"
+      puts "Connecting to #{host}..."
+
+      id_pub_content = File.read File.expand_path("~/.ssh/id_rsa.pub")
+
+      puts "copying id to remote host #{id_pub_content}"
+
+      Net::SSH.start(host, user, {:password => password, :port => 2222}) do |ssh|
+        # capture all stderr and stdout output from a remote process
+        remote_host_name = ssh.exec!("hostname")
+
+        puts "Connected to #{remote_host_name}"
+
+        # capture only stdout matching a particular pattern
+        stdout = ""
+        stderr = ""
+        ssh.exec!("echo '#{id_pub_content}' >> ~/.ssh/authorized_keys") do |channel, stream, data|
+          stdout << data if stream == :stdout
+          stderr << data if stream == :stderr
+        end
+        puts stdout
+        puts stderr
+
+      end
+
     end
   end
 end
--- marhan_cli.gemspec
+++ marhan_cli.gemspec
@@ -18,4 +18,5 @@ Gem::Specification.new do |gem|
   gem.require_paths = ["lib"]
 
   gem.add_runtime_dependency('thor')
+  gem.add_runtime_dependency('net-ssh', '~> 2.6.1')
 end
